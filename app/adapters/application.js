bd31e8e747a2736f193afb123f07df9d296b73c9
naveena-zstk222 <TSI-ZOHOCORP\naveena-zstk222@sysadmin-dell-u20.csez.zohocorpin.com>
2021-05-10 10:10:44 +0530

    removed unwanted files

--- app/adapters/application.js
+++
@@ -1,4 +0,0 @@
-import RESTAdapter from '@ember-data/adapter/json-api';
-
-export default class ApplicationAdapter extends RESTAdapter {
-}
--- app/models/category.js
+++ app/models/category.js
@@ -1,6 +1,5 @@
 import Model ,{attr, hasMany}from '@ember-data/model';
 
 export default class CategoryModel extends Model {
-  @hasMany('recipe') recipe;
   @attr('string') categoryName;
 }
